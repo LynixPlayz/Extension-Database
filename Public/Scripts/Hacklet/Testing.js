0860f619c7db835d70e2b8d5ae7bf0418b7aa35f
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-11-03 11:38:52 +0000

    Update Testing.js

--- Public/Scripts/Hacklet/Testing.js
+++ Public/Scripts/Hacklet/Testing.js
@@ -1,10 +1,8 @@
 // -- Hacklet --
 // @name         Testing
-// @version      1.0
+// @version      0.1
 // @author       Hacklet
 // @urls         ["https://play.blooket.com/", "https://play.blooket.com/*"]
 // -- Hacklet -- 
 
-window.addEventListener('load', function () {
-  alert("hi")
-});
+alert("hi")
