b0eddaa7f1015f32c88df80dc82b94ad3e7ad43d
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-11-03 11:12:59 +0000

    Create Testing.js

---
+++ Public/Scripts/Hacklet/Testing.js
@@ -0,0 +1,10 @@
+// -- Hacklet --
+// @name         Testing
+// @version      1.0
+// @author       Hacklet
+// @urls         ["https://play.blooket.com/", "https://play.blooket.com/*"]
+// -- Hacklet -- 
+
+window.addEventListener('load', function () {
+  alert("hi")
+});
